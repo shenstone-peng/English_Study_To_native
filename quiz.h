18bf97ee34a0dd4168d9253a14dca8132c5f560a
shenstone-peng <489439220@qq.com>
2022-06-28 18:14:36 +0800

    Update quiz.h

--- quiz.h
+++ quiz.h
@@ -10,37 +10,44 @@
 #include <ctime>
 #include <fstream>
 
-
+using namespace std;
 namespace SP {
     
 class quiz {
+public:
     typedef std::string FILE_NAME;
     typedef std::vector<FILE_NAME> FILE_NAME_LIST;
     typedef std::string SENTENCE;
+    typedef int NUM;
     typedef enum _FILE_OPERATE
     {
         NO_ERR = 0,
         ILEGAL_FORMAT,
         OPEN_DIR_FAIL,
+	EMPTY_DATA,
     }FILE_OPERATE;
     public:
-    quiz(const& string i_filepath):file_path(i_filepath){
+    quiz(const string& i_filepath):mStrFilePath(i_filepath){
         std::cout<<"new quiz from path ["<<i_filepath<<"]"<<endl;
         unsigned int seed = time(0);
         srand(seed);
     }
     FILE_OPERATE initFileList();
+    
     FILE_OPERATE getRandomFile(FILE_NAME& o_fileName);
+    
     SENTENCE getRandomSentence(const FILE_NAME& i_fileName);
+    
+    int getRandomNumber(vector<int>& i_list);
     bool isFilePath(const std::string& i_filepath);
     
 
     
 private:
-    std::string file_path;
-    std::map<FILE_NAME, vector<SENTENCE> dictionary;
-    FILE_NAME_LIST fileList;
-}
+    std::string mStrFilePath;
+    std::map<FILE_NAME, vector<pair<SENTENCE, NUM>>> mMapDictionary;
+    FILE_NAME_LIST mVecFileList;
+};
 
 
 }//end namespace
