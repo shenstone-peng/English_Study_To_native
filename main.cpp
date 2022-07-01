b9f6a45da30575cfc69330fabf157e0e69e3f735
shenstone-peng <489439220@qq.com>
2022-07-01 13:46:12 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,25 +13,31 @@ int main(int argc, char *argv[]){
     	}
 	SP::quiz* tmp = new SP::quiz(filePath);
 	(void)tmp->initFileList();
+	tmp->readJsonFile();
 	int i = 0;
-	while(i++ < 20){
+	while(i++ < 19){
 		SP::quiz::FILE_NAME choosedFile="";
 		(void)tmp->getRandomFile(choosedFile);
-		cout<<tmp->getRandomSentence(filePath+choosedFile)<<endl;
+		cout<<"**************No."<<i<<"******************"<<endl;
+		string sentence = tmp->getRandomSentence(filePath+choosedFile);
+		cout<<"*   "<<sentence<<"    *"<<endl;
 		cout<<endl;
-		cout<<"************Y/N***************************"<<endl;
+		cout<<"[Y/N]                                     "<<endl;
 		char c;
 		cin.get(c);
 		if('y' == c || 'Y' ==c){
         	        cout<<"Congrates!!"<<endl;
+			tmp->updateMap(filePath+choosedFile, sentence, -1);
 	        }
         	else{
                		cout<<"Sorry~"<<endl;
+			tmp->updateMap(filePath+choosedFile, sentence, 1);
 	        }
-        	std::cout<<"Press enter to next WORD..."<<endl;
+        	cout<<"******Press enter to next WORD...*********"<<endl;
 	        std::cin.ignore(1);
         	cin.get();
 	}
+	(void)tmp->generateNewJsonFile();
 	delete tmp;
 	return 0;
 }
